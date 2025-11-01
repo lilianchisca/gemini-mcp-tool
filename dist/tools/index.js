323cb505e66947c0140bb3d1d8284385cfc3dfcc
Lilian Chisca <lilian.chisca@gmail.com>
2025-11-01 16:43:45 +0200

    add dist to git

---
+++ dist/tools/index.js
@@ -0,0 +1,6 @@
+// Tool Registry Index - Registers all tools
+import { toolRegistry } from './registry.js';
+import { askGeminiTool } from './ask-gemini.tool.js';
+toolRegistry.push(askGeminiTool);
+export * from './registry.js';
+//# sourceMappingURL=index.js.map
